636ece0fd47611cef098ff19e4c0ee9c41f44d0d
Tropped <tropped@gmail.com>
2023-01-07 01:14:02 +0100

    Refactored the table in the generator and included Videogames

--- renderer/js/edit-list.js
+++ renderer/js/edit-list.js
@@ -1,7 +1,8 @@
 const { alertError } = alerts;
 
 // Get Elements
-const rowGroup = document.getElementById("table-row-group");
+
+const typeSelect = document.getElementById("type-select");
 
 const btnAddRow = document.getElementById("btn-add-row");
 const btnSave = document.getElementById("btn-save");
@@ -22,19 +23,19 @@ let highlightedRowNumber = -1;
 const lbNumSongs = document.getElementById("num-songs");
 
 /**
- * Returns the HTML of the new table row
+ * Returns the HTML of the new table row for anime
  * @param {int} i Number of row
  * @returns HTML code of the new row in the table
  */
-function newRow(i) {
-  return `<div id="row-${i}" class="table-row">
-          <div class="table-cell pl-2 text-slate-500">
-            <img id="delete-${i}" name="delete" src="./images/x.png" width="10" class="cursor-pointer">
-          </div>
-          <div class="table-cell p-0 text-slate-500 ">
+function newAnimeRow(i) {
+  return `<tr id="anime-row-${i}" class="w-full">
+          <td class="pl-2 text-slate-500">
+            <img id="delete-anime-${i}" name="delete" src="./images/x.png" width="10" class="cursor-pointer">
+          </td>
+          <td class="p-0 text-slate-500 ">
             <input id="anime-${i}" name="anime" class="pl-2 w-full bg-slate-100" type="text" spellcheck="false" placeholder="Nombre del anime...">
-          </div>
-          <div class="table-cell pl-2 text-slate-500 text-center">
+          </td>
+          <td class="pl-2 text-slate-500 text-center">
             <span>
               <select id="op-ed-${i}" class="bg-slate-100 w-5/12">
                 <option value="op">Opening</option>
@@ -43,28 +44,113 @@ function newRow(i) {
               </select>
               <input id="op-ed-num-${i}" class="w-12 bg-slate-100" type="number" min="1" value="1">
             </span>
-          </div>
-          <div class="table-cell p-0 text-slate-500">
+          </td>
+          <td class="p-0 text-slate-500">
             <input id="band-${i}" name="band" type="text" class="bg-slate-100 pl-2 w-full" spellcheck="false" placeholder="Artista...">
-          </div>
-          <div class="table-cell p-0 text-slate-500">
+          </td>
+          <td class="p-0 text-slate-500">
             <input id="song-${i}" name="song" type="text" class="bg-slate-100 pl-2 w-full" spellcheck="false" placeholder="Nombre de la canción...">
-          </div>
-          <div class="table-cell pl-2 text-slate-500 text-center">
-            <select id="difficulty-${i}" class="bg-slate-100" name="difficulty">
+          </td>
+          <td class="pl-2 text-slate-500 text-center">
+            <select id="difficulty-anime-${i}" class="bg-slate-100" name="difficulty">
               <option value="hard">Hard</option>
               <option selected value="normal">Normal</option>
               <option value="easy">Easy</option>
             </select>
-          </div>
-          <div class="table-cell p-0">
-            <input id="yt-link-${i}" name="yt-link" class="bg-slate-100 pl-2 pr-2 text-slate-500 w-full" spellcheck="false" placeholder="Enlace de youtube...">
-          </div>
-        </div>`;
+          </td>
+          <td class="p-0">
+            <input id="yt-link-anime-${i}" name="yt-link" class="bg-slate-100 pl-2 pr-2 text-slate-500 w-full" spellcheck="false" placeholder="Enlace de youtube...">
+          </td>
+        </tr>`;
+}
+
+/**
+ * Returns the HTML of the new table row
+ * @param {int} i Number of row
+ * @returns HTML code of the new row in the table
+ */
+function newGameRow(i) {
+  return `<tr id="game-row-${i}">
+          <td class="pl-2 text-slate-500">
+            <img id="delete-game-${i}" name="delete" src="./images/x.png" width="10" class="cursor-pointer">
+          </td>
+          <td class="p-0 text-slate-500 ">
+            <input id="saga-${i}" name="saga" class="pl-2 w-full bg-slate-100" type="text" spellcheck="false" placeholder="Saga...">
+          </td>
+          <td class="p-0 text-slate-500">
+            <input id="game-${i}" name="band" type="text" class="bg-slate-100 pl-2 w-full" spellcheck="false" placeholder="Juego...">
+          </td>
+          <td class="p-0 text-slate-500">
+            <input id="description-${i}" name="description" type="text" class="bg-slate-100 pl-2 w-full" spellcheck="false" placeholder="Descripción de la situación, nombre...">
+          </td>
+          <td class="pl-2 text-slate-500 text-center">
+            <select id="difficulty-game-${i}" class="bg-slate-100" name="difficulty">
+              <option value="hard">Hard</option>
+              <option selected value="normal">Normal</option>
+              <option value="easy">Easy</option>
+            </select>
+          </td>
+          <td class="p-0">
+            <input id="yt-link-game-${i}" name="yt-link" class="yt-link bg-slate-100 pl-2 pr-2 text-slate-500 w-full" spellcheck="false" placeholder="Enlace de youtube...">
+          </td>
+        </tr>`;
 }
 
 function parseJson() {
   return JSON.parse(fs.readFileSync(filePath, "utf-8").toString());
+}
+
+function switchTable(type) {
+  const animeHeader = document.getElementById("anime-table-header");
+  const gameHeader = document.getElementById("game-table-header");
+
+  const animeRowGroup = document.getElementById("anime-table-row-group");
+  const gameRowGroup = document.getElementById("game-table-row-group");
+
+  const rows = getRowGroup(type).getElementsByTagName("tr");
+  lbNumSongs.innerHTML = rows.length;
+
+  if (type === "Anime") {
+    animeHeader.classList.remove("hidden");
+    animeRowGroup.classList.remove("hidden");
+
+    gameHeader.classList.add("hidden");
+    gameRowGroup.classList.add("hidden");
+  } else {
+    animeHeader.classList.add("hidden");
+    animeRowGroup.classList.add("hidden");
+
+    gameHeader.classList.remove("hidden");
+    gameRowGroup.classList.remove("hidden");
+  }
+}
+
+function fillRow(i, song, type) {
+  if (type === "Anime") fillAnimeRow(i, song);
+  else fillGameRow(i, song);
+}
+
+function fillAnimeRow(i, song) {
+  const { anime, oped, opedNumber, band, name, difficulty, link } = song;
+
+  document.getElementById(`anime-${i}`).value = anime;
+  document.getElementById(`op-ed-${i}`).value =
+    oped === "Opening" ? "op" : oped === "Ending" ? "ed" : "ost";
+  document.getElementById(`op-ed-num-${i}`).value = opedNumber;
+  document.getElementById(`band-${i}`).value = band;
+  document.getElementById(`song-${i}`).value = name;
+  document.getElementById(`difficulty-anime-${i}`).value = difficulty;
+  document.getElementById(`yt-link-anime-${i}`).value = link;
+}
+
+function fillGameRow(i, song) {
+  const { saga, game, description, difficulty, link } = song;
+
+  document.getElementById(`saga-${i}`).value = saga;
+  document.getElementById(`game-${i}`).value = game;
+  document.getElementById(`description-${i}`).value = description;
+  document.getElementById(`difficulty-game-${i}`).value = difficulty;
+  document.getElementById(`yt-link-game-${i}`).value = link;
 }
 
 /**
@@ -73,58 +159,50 @@ function parseJson() {
 function initTable() {
   if (!filePath || filePath === "" || !filePath.endsWith(".json")) return;
 
-  const { author, songs } = parseJson();
+  const { type, author, songs } = parseJson();
 
   lbNumSongs.innerHTML = songs.length;
 
   document.getElementById("list-author").value = author;
 
+  typeSelect.value = type;
+
+  switchTable(type);
   songs.forEach((song, i) => {
-    addNewRow();
-
-    const { anime, oped, opedNumber, band, name, difficulty, link } = song;
-
-    const animeField = document.getElementById(`anime-${i}`);
-    animeField.value = anime;
-
-    const openingField = document.getElementById(`op-ed-${i}`);
-    openingField.value =
-      oped === "Opening" ? "op" : oped === "Ending" ? "ed" : "ost";
-
-    const openingNumField = document.getElementById(`op-ed-num-${i}`);
-    openingNumField.value = opedNumber;
-
-    const bandField = document.getElementById(`band-${i}`);
-    bandField.value = band;
-
-    const songField = document.getElementById(`song-${i}`);
-    songField.value = name;
-
-    const difficultyField = document.getElementById(`difficulty-${i}`);
-    difficultyField.value = difficulty;
-
-    const linkField = document.getElementById(`yt-link-${i}`);
-    linkField.value = link;
+    addNewRow(type);
+    fillRow(i, song, type);
   });
+}
+
+function getRowGroup(type) {
+  return document.getElementById(`${type.toLowerCase()}-table-row-group`);
 }
 
 /**
  * Adds a new empty row
  */
-function addNewRow() {
-  const rows = rowGroup.getElementsByClassName("table-row");
+function addNewRow(type) {
+  type = type.toLowerCase();
+  const rowGroup = getRowGroup(type);
+
+  const rows = rowGroup.getElementsByTagName("tr");
   let newRowNumber = rows.length;
   if (rows.length > 0) {
     newRowNumber = parseInt(Array.from(rows).at(-1).id.split("-").at(-1)) + 1;
   }
-  rowGroup.insertAdjacentHTML("beforeend", newRow(newRowNumber));
+
+  const newRowHtml =
+    type === "anime" ? newAnimeRow(newRowNumber) : newGameRow(newRowNumber);
+
+  rowGroup.insertAdjacentHTML("beforeend", newRowHtml);
 
   lbNumSongs.innerHTML = rows.length;
 
-  const x = document.getElementById(`delete-${newRowNumber}`);
+  const x = document.getElementById(`delete-${type}-${newRowNumber}`);
   x.addEventListener("click", deleteRow);
 
-  const newRowElement = document.getElementById(`row-${newRowNumber}`);
+  const newRowElement = document.getElementById(`${type}-row-${newRowNumber}`);
+
   const inputs = newRowElement.querySelectorAll("input, select");
   inputs.forEach((input) => input.addEventListener("focus", focusRow));
 }
@@ -135,9 +213,10 @@ function addNewRow() {
  */
 function deleteRow(e) {
   const rowNumber = e.target.id.split("-").at(-1);
-  const row = document.getElementById(`row-${rowNumber}`);
+  const type = typeSelect.value.toLowerCase();
+  const row = document.getElementById(`${type}-row-${rowNumber}`);
 
-  const rows = rowGroup.getElementsByClassName("table-row");
+  const rows = getRowGroup(type).getElementsByTagName("tr");
   row.remove();
   lbNumSongs.innerHTML = rows.length;
 }
@@ -153,15 +232,16 @@ function validate() {
     return false;
   }
 
-  // Anime is not empty
-  const animes = Array.from(document.getElementsByName("anime"));
-  if (animes.some((anime) => anime.value.trim() == "")) {
-    alertError("El campo Anime es obligatorio.");
-    return false;
+  const type = typeSelect.value;
+
+  if (type == "Anime") {
+    if (!validateAnime()) return false;
   }
 
   // Youtube is not empty
-  const links = Array.from(document.getElementsByName("yt-link"));
+
+  const rowGroup = getRowGroup(type);
+  const links = Array.from(rowGroup.getElementsByClassName("yt-link"));
   if (links.some((link) => link.value.trim() == "")) {
     alertError("El campo Youtube es obligatorio.");
     return false;
@@ -184,12 +264,27 @@ function validate() {
   return true;
 }
 
+function validateAnime() {
+  // Anime is not empty
+  const animes = Array.from(document.getElementsByName("anime"));
+  if (animes.some((anime) => anime.value.trim() == "")) {
+    alertError("El campo Anime es obligatorio.");
+    return false;
+  }
+}
+
+function songConstructor(rowNumber) {
+  const type = typeSelect.value;
+  if (type == "Anime") return animeSongConstructor(rowNumber);
+  else return gameSongConstructor(rowNumber);
+}
+
 /**
  * Build a song from its row number
  * @param {int} rowNumber
  * @returns Song object
  */
-function songConstructor(rowNumber) {
+function animeSongConstructor(rowNumber) {
   const opedValue = document.getElementById(`op-ed-${rowNumber}`).value;
   let oped = "";
   if (opedValue === "op") oped = "Opening";
@@ -202,8 +297,20 @@ function songConstructor(rowNumber) {
     opedNumber: document.getElementById(`op-ed-num-${rowNumber}`).value,
     band: document.getElementById(`band-${rowNumber}`).value.trim(),
     name: document.getElementById(`song-${rowNumber}`).value.trim(),
-    difficulty: document.getElementById(`difficulty-${rowNumber}`).value,
-    link: document.getElementById(`yt-link-${rowNumber}`).value.trim(),
+    difficulty: document.getElementById(`difficulty-anime-${rowNumber}`).value,
+    link: document.getElementById(`yt-link-anime-${rowNumber}`).value.trim(),
+  };
+}
+
+function gameSongConstructor(rowNumber) {
+  return {
+    saga: document.getElementById(`saga-${rowNumber}`).value.trim(),
+    game: document.getElementById(`game-${rowNumber}`).value.trim(),
+    description: document
+      .getElementById(`description-${rowNumber}`)
+      .value.trim(),
+    difficulty: document.getElementById(`difficulty-game-${rowNumber}`).value,
+    link: document.getElementById(`yt-link-game-${rowNumber}`).value.trim(),
   };
 }
 
@@ -213,15 +320,17 @@ function songConstructor(rowNumber) {
  */
 function saveFile() {
   if (!validate()) return;
-  const rows = rowGroup.getElementsByClassName("table-row");
+  const type = typeSelect.value;
+  const rows = getRowGroup(type).getElementsByTagName("tr");
   const songArray = Array.from(rows).map((row) => {
     const rowNumber = row.id.split("-").at(-1);
     return songConstructor(rowNumber);
   });
 
-  const author = document.getElementById("list-author").value;
+  const author = document.getElementById("list-author").value.trim();
 
   const trivialList = {
+    type,
     author,
     songs: songArray,
   };
@@ -240,7 +349,8 @@ function goBack() {
 }
 
 initTable();
-btnAddRow.addEventListener("click", addNewRow);
+switchTable(typeSelect.value);
+btnAddRow.addEventListener("click", (e) => addNewRow(typeSelect.value));
 btnSave.addEventListener("click", saveFile);
 btnCancel.addEventListener("click", goBack);
 
@@ -253,7 +363,8 @@ ipcRenderer.on("dialog:listTargetPath", (params) => {
  * @param {*} rowNumber Number of the row to highlight
  */
 function highlightRow(rowNumber) {
-  const row = document.getElementById(`row-${rowNumber}`);
+  const type = typeSelect.value;
+  const row = document.getElementById(`${type.toLowerCase()}-row-${rowNumber}`);
 
   row.classList.remove("bg-slate-100");
   row.classList.add("bg-slate-400");
@@ -271,8 +382,11 @@ function highlightRow(rowNumber) {
 /**
  * Lowlights the row highlighted
  */
-function lowlightRow() {
-  const row = document.getElementById(`row-${highlightedRowNumber}`);
+function lowlightRow(type = null) {
+  type = type ?? typeSelect.value;
+  const row = document.getElementById(
+    `${type.toLowerCase()}-row-${highlightedRowNumber}`
+  );
   row.classList.remove("bg-slate-400");
   row.classList.remove("text-slate-800");
   row.classList.add("bg-slate-100");
@@ -293,3 +407,14 @@ function focusRow(e) {
 
   highlightedRowNumber = rowNumber;
 }
+
+typeSelect.addEventListener("change", (e) => {
+  const newType = e.target.value;
+  const oldType = newType == "Anime" ? "Game" : "Anime";
+
+  if (highlightedRowNumber >= 0) {
+    lowlightRow(oldType);
+    highlightedRowNumber = -1;
+  }
+  switchTable(newType);
+});
